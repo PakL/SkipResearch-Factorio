6e699c6cdb718b031d24012d1e17e6e4e3d1a71a
PakL <webmaster@paklweb.de>
2020-05-19 07:52:29 +0200

    Some additional changes to make it more resistant to errors

--- control.lua
+++ control.lua
@@ -1,22 +1,33 @@
 local askagain = true
 local skipresearches = false
 
+-- Handle GUI click events
 script.on_event(defines.events.on_gui_click, function(event)
-	if game.players[event.player_index].force.current_research ~= nil then
+	-- Check if research is even running
+	-- Remove GUI to make sure
+	if game.players[event.player_index].force.current_research == nil then
+		if game.players[event.player_index].gui.top.skipresearch_tbl ~= nil then
+			game.players[event.player_index].gui.top.skipresearch_tbl.destroy()
+		end
 		return
 	end
 
+	local skipthistime = false
+	local skipthemall = false
+	-- Skip a single research?
 	if event.element.name == "skipresearch_yes" then
+		skipthistime = true
 		skipresearches = true
-		game.players[event.player_index].force.research_progress = 1
 		if game.players[event.player_index].gui.top.skipresearch_tbl.skipresearch_btns.skipresearch_mem.state then
 			askagain = false
 		end
+	-- Skip all researches?
 	elseif event.element.name == "skipresearch_all" then
 		skipresearches = true
 		askagain = false
-		game.players[event.player_index].force.research_progress = 1
-		game.players[event.player_index].force.research_all_technologies()
+		skipthistime = true
+		skipthemall = true
+	-- Don't skip the research?
 	elseif event.element.name == "skipresearch_no" then
 		skipresearches = false
 		if game.players[event.player_index].gui.top.skipresearch_tbl.skipresearch_btns.skipresearch_mem.state then
@@ -24,13 +35,25 @@ script.on_event(defines.events.on_gui_click, function(event)
 		end
 	end
 
-	if event.element.name ~= "skipresearch_mem" then
+	if skipthistime then
+		-- Set research progress to 100%
+		game.players[event.player_index].force.research_progress = 1
+	elseif event.element.name == "skipresearch_no" then
+		-- Remove gui
 		for i,plyr in pairs(game.players) do
-			if not plyr.gui.top.skipresearch_tbl then else plyr.gui.top.skipresearch_tbl.destroy() end
+			if plyr.gui.top.skipresearch_tbl ~= nil then
+				plyr.gui.top.skipresearch_tbl.destroy()
+			end
 		end
+	end
+	if skipthemall then
+		-- Skip all researches
+		game.players[event.player_index].force.research_all_technologies()
 	end
 end)
 
+-- Create gui on new research
+-- No gui will be created if the gui is not to be shown again
 script.on_event(defines.events.on_research_started, function(event)
 	if askagain then
 		for i,plyr in pairs(game.players) do
@@ -51,6 +74,7 @@ script.on_event(defines.events.on_research_started, function(event)
 	end
 end)
 
+-- Remove gui for all players if research is finished
 script.on_event(defines.events.on_research_finished, function(event)
 	for i,plyr in pairs(game.players) do
 		if plyr.gui.top.skipresearch_tbl ~= nil then
