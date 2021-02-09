e8a9ce090fe8f9df74736d74a1236e02488f977e
PakL <webmaster@paklweb.de>
2021-02-09 11:39:37 +0100

    Added research timer with settings

--- control.lua
+++ control.lua
@@ -1,5 +1,7 @@
-local askagain = true
 local skipresearches = false
+local skipthistime = false
+local skipstartclock = 0
+local skipresearch_research = nil
 
 -- Handle GUI click events
 script.on_event(defines.events.on_gui_click, function(event)
@@ -12,41 +14,46 @@ script.on_event(defines.events.on_gui_click, function(event)
 		return
 	end
 
-	local skipthistime = false
+	if event.element.name == "skipresearch_mem" then
+		return
+	end
+
 	local skipthemall = false
 	-- Skip a single research?
 	if event.element.name == "skipresearch_yes" then
 		skipthistime = true
 		skipresearches = true
 		if game.players[event.player_index].gui.top.skipresearch_tbl.skipresearch_btns.skipresearch_mem.state then
+			settings.global["skip-research-askagain"] = {value = false}
 			askagain = false
 		end
 	-- Skip all researches?
 	elseif event.element.name == "skipresearch_all" then
 		skipresearches = true
-		askagain = false
-		skipthistime = true
+		settings.global["skip-research-askagain"] = {value = false}
+		skipthistime = false
 		skipthemall = true
 	-- Don't skip the research?
 	elseif event.element.name == "skipresearch_no" then
 		skipresearches = false
 		if game.players[event.player_index].gui.top.skipresearch_tbl.skipresearch_btns.skipresearch_mem.state then
-			askagain = false
+			settings.global["skip-research-askagain"] = {value = false}
+		end
+	end
+
+	-- Remove gui
+	for i,plyr in pairs(game.players) do
+		if plyr.gui.top.skipresearch_tbl ~= nil then
+			plyr.gui.top.skipresearch_tbl.destroy()
 		end
 	end
 
 	if skipthistime then
 		-- Set research progress to 100%
-		game.players[event.player_index].force.research_progress = 1
-	elseif event.element.name == "skipresearch_no" then
-		-- Remove gui
-		for i,plyr in pairs(game.players) do
-			if plyr.gui.top.skipresearch_tbl ~= nil then
-				plyr.gui.top.skipresearch_tbl.destroy()
-			end
-		end
+		skipstartclock = game.ticks_played
 	end
 	if skipthemall then
+		skipresearch_research = nil
 		-- Skip all researches
 		game.players[event.player_index].force.research_all_technologies()
 	end
@@ -55,7 +62,8 @@ end)
 -- Create gui on new research
 -- No gui will be created if the gui is not to be shown again
 script.on_event(defines.events.on_research_started, function(event)
-	if askagain then
+	skipresearch_research = event.research
+	if settings.global["skip-research-askagain"].value then
 		for i,plyr in pairs(game.players) do
 			if plyr.gui.top.skipresearch_tbl ~= nil then
 				plyr.gui.top.skipresearch_tbl.destroy()
@@ -69,8 +77,8 @@ script.on_event(defines.events.on_research_started, function(event)
 			plyr.gui.top.skipresearch_tbl.add{type="button", name="skipresearch_all", caption={"skip-research-all"}}
 		end
 	elseif skipresearches then
-		local event_research = event.research
-		event_research.force.research_progress = 1
+		skipthistime = true
+		skipstartclock = game.ticks_played
 	end
 end)
 
@@ -79,6 +87,31 @@ script.on_event(defines.events.on_research_finished, function(event)
 	for i,plyr in pairs(game.players) do
 		if plyr.gui.top.skipresearch_tbl ~= nil then
 			plyr.gui.top.skipresearch_tbl.destroy()
+		end
+	end
+end)
+
+-- Update research progress
+script.on_event(defines.events.on_tick, function(event)
+	if skipthistime and skipresearch_research ~= nil then
+		if skipresearch_research.researched then
+			skipthistime = false
+			skipresearch_research = nil
+			return
+		end
+
+		local now = game.ticks_played * game.speed / 60
+		local sthen = skipstartclock * game.speed / 60
+		local diff = now - sthen
+		if diff >= settings.global["skip-research-timeout"].value then
+			skipresearch_research.force.research_progress = 1
+			skipthistime = false
+			skipresearch_research = nil
+		else
+			local progress = 1 / settings.global["skip-research-timeout"].value * diff
+			if progress < 1 then
+				skipresearch_research.force.research_progress = progress
+			end
 		end
 	end
 end)
---
+++ settings.lua
@@ -0,0 +1,15 @@
+data:extend({
+	{
+		type = "int-setting",
+		name = "skip-research-timeout",
+		setting_type = "runtime-global",
+		minimum_value = 0,
+		default_value = 0
+	},
+	{
+		type = "bool-setting",
+		name = "skip-research-askagain",
+		setting_type = "runtime-global",
+		default_value = true
+	}
+})
